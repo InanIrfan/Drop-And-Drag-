fd2105b54e44432ad89d20b1f6ad1e9aed19ccd7
InanIrfan <129646901+InanIrfan@users.noreply.github.com>
2024-11-24 14:58:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var draggedElement = document.getElementById(data);
+    var container = document.querySelector('.container');
+    container.appendChild(draggedElement);
+}
